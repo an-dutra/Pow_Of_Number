f0c29c61b7cd4c7626055fa15dd6fa23a2ab137c
an-dutra <40439091+an-dutra@users.noreply.github.com>
2021-07-06 20:23:26 -0300

    Update is_pow.py
    Some new updates to improve results

--- is_pow.py
+++ is_pow.py
@@ -1,16 +1,29 @@
-import math 
+import math
+# Given an integer n, return true if it is a power of ten. Otherwise, return false.
+# An integer n is a power of ten, if there exists an integer x such that n == 10^x
 
 def is_natural_number(x):
+    if x == 0:
+        return True
+    elif int(x) == 0:
+        return False
     return x/int(x) == 1
 
 
 def is_pow_of_base(number, base, precision=5):
+    if number <= 0:
+        return False
     result = round(math.log(number, base),5)
     return is_natural_number(result)
 
-def is_pow_of_ten(number):
+def is_power_of_ten(number):
     return is_pow_of_base(number, 10)
-    
-if __name__ == '__main__':
-    print(is_pow_of_ten(100))
 
+if __name__ == "__main__":
+    print(is_power_of_ten(1))  # true
+    print(is_power_of_ten(10))  # true
+    print(is_power_of_ten(1000))  # true
+    print(is_power_of_ten(5))  # false
+    print(is_power_of_ten(20))  # false
+    print(is_power_of_ten(-1))  # false
+    print(is_power_of_ten(0.00001))  # true
